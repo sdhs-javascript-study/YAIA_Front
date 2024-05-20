76ff064a71cbbf2753b824c1362b25bb81661997
moonlight060426 <leehyina640@gmail.com>
2024-05-20 11:16:50 +0900

    javascript 코드 수정

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -4,31 +4,31 @@ document.querySelector(".posting").addEventListener("click", () => {
 
   if (!question && !nickname) {
     alert("질문과 닉네임을 입력하지 않았습니다.");
-    return;
-  }
-
-  if (!question) {
+  } else if (!question) {
     alert("질문을 입력해주세요.");
-    return;
-  }
-
-  if (!nickname) {
+  } else if (!nickname) {
     alert("닉네임을 입력해주세요.");
-    return;
+  } else {
+    const options = {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+      timeZone: "Asia/Seoul",
+    };
+    const currentTime = new Date().toLocaleString("ko-KR", options);
+    const postSection = document.getElementById("posts");
+    postSection.classList.remove("hidden");
+
+    postSection.innerHTML = `
+            <span class="first_question">${question}</span>
+            <div class="post_bundle">
+                <img src="../img/green_user.png" alt="green_user" class="green_user">
+                <span id="nickname_rabbit">${nickname}</span>
+                <p class="date">${currentTime}</p>
+            </div>
+        `;
   }
-
-  const currentTime = new Date().toLocaleString("ko-KR", {
-    timeZone: "Asia/Seoul",
-  });
-  const postSection = document.getElementById("posts");
-  postSection.classList.remove("hidden");
-
-  postSection.innerHTML = `
-                <span class="first_question">${question}</span>
-                <div class="post_bundle">
-                    <img src="../img/green_user.png" alt="green_user" class="green_user"> <!-- 이미지 위치 변경 -->
-                    <span id="nickname_rabbit">${nickname}</span>
-                    <p class="date">${currentTime}</p>
-                </div>
-            `;
 });
