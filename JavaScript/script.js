eb2409146c796999a413ad8f1a490fd2e7990298
moonlight060426 <leehyina640@gmail.com>
2024-05-20 11:12:43 +0900

    main_html 수정

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,26 +1,34 @@
 document.querySelector(".posting").addEventListener("click", () => {
-  const question = document.querySelector(".question").value;
-  const nickname = document.querySelector("#nickname").value || "익명";
+  const question = document.querySelector(".question").value.trim();
+  const nickname = document.querySelector("#nickname").value.trim();
+
+  if (!question && !nickname) {
+    alert("질문과 닉네임을 입력하지 않았습니다.");
+    return;
+  }
+
+  if (!question) {
+    alert("질문을 입력해주세요.");
+    return;
+  }
+
+  if (!nickname) {
+    alert("닉네임을 입력해주세요.");
+    return;
+  }
+
   const currentTime = new Date().toLocaleString("ko-KR", {
     timeZone: "Asia/Seoul",
   });
-
   const postSection = document.getElementById("posts");
-  if (postSection) {
-    postSection.insertAdjacentHTML(
-      "beforeend",
-      `
-                    <div class="post_bundle">
-                        <img src="../img/green_user.png" alt="green_user" class="green_user"> <!-- 이미지 위치 변경 -->
-                        <div class="post_content">
-                            <span>${nickname}</span>
-                            <p>${question}</p>
-                        </div>
-                        <div class="post_actions">
-                            <p class="date">${currentTime}</p>
-                        </div>
-                    </div>
-                `
-    );
-  }
+  postSection.classList.remove("hidden");
+
+  postSection.innerHTML = `
+                <span class="first_question">${question}</span>
+                <div class="post_bundle">
+                    <img src="../img/green_user.png" alt="green_user" class="green_user"> <!-- 이미지 위치 변경 -->
+                    <span id="nickname_rabbit">${nickname}</span>
+                    <p class="date">${currentTime}</p>
+                </div>
+            `;
 });
