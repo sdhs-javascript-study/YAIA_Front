262ee60620560a484d095590f22552c6ec2389e5
moonlight060426 <leehyina640@gmail.com>
2024-05-20 10:03:36 +0900

    main javascript upgrade

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,26 @@
-// init
+document.querySelector(".posting").addEventListener("click", () => {
+  const question = document.querySelector(".question").value;
+  const nickname = document.querySelector("#nickname").value || "익명";
+  const currentTime = new Date().toLocaleString("ko-KR", {
+    timeZone: "Asia/Seoul",
+  });
+
+  const postSection = document.getElementById("posts");
+  if (postSection) {
+    postSection.insertAdjacentHTML(
+      "beforeend",
+      `
+                    <div class="post_bundle">
+                        <img src="../img/green_user.png" alt="green_user" class="green_user"> <!-- 이미지 위치 변경 -->
+                        <div class="post_content">
+                            <span>${nickname}</span>
+                            <p>${question}</p>
+                        </div>
+                        <div class="post_actions">
+                            <p class="date">${currentTime}</p>
+                        </div>
+                    </div>
+                `
+    );
+  }
+});
